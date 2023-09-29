dd2d2dbff8ec51f2f312fa54fd01834a93ebc756
ichwansh03 <ichwansholihin03@gmail.com>
2023-09-29 12:59:08 +0700

    chore: add map collections

---
+++ src/main/java/org/example/collection/MapApp.java
@@ -0,0 +1,34 @@
+package org.example.collection;
+
+import java.util.*;
+
+public class MapApp {
+    public static void main(String[] args) {
+        Map<Integer, String> maps = new HashMap<>();
+        //Map<Integer, String> maps = new IdentityHashMap<>();
+        //Map<Integer, String> maps = new LinkedHashMap<>();
+        //Map<Integer, String> maps = new WeakHashMap<>();
+        maps.put(1,"First");
+        maps.put(2,"Second");
+        maps.put(3,"Third");
+        maps.put(1,"Pertama");
+
+        for (var key: maps.keySet()) {
+            System.out.println(key);
+        }
+
+        for (var value: maps.values()) {
+            System.out.println(value);
+        }
+
+        entries(maps);
+    }
+
+    public static void entries(Map<Integer, String> map){
+        Set<Map.Entry<Integer, String>> entries = map.entrySet();
+
+        for (var entry : entries){
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+        }
+    }
+}
